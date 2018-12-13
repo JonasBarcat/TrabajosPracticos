20b1c961e2b97a02b0f2741609c3fdb174a39e6f
Gianpaolo <Gianpaolo@DESKTOP-I1OSCDA>
2018-12-12 21:49:00 -0300

    Commit 2 Clase ejemplo

---
+++ src/unt/herrera/prog2/tp5/ClaseEjemplo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unt.herrera.prog2.tp5;
+
+/**
+ *
+ * @author Gianpaolo
+ */
+public class ClaseEjemplo {
+    
+}
